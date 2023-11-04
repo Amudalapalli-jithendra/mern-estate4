f79cb52a34ec6b23044fcc7143612b53de20bd2b
jithendra <jithendra989@gmail.com>
2023-11-04 19:10:41 +0530

    add redux toolkit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -30,8 +30,6 @@ export default function SignUp() {
   if(data.success === false) {
     setLoading(false);
     setError(data.message);
-    
-    
     return;
   }
   setLoading(false);
@@ -41,8 +39,6 @@ export default function SignUp() {
      setLoading(false);
      setError(error.message); 
     }
-    
-
   };
  
   return (
@@ -50,20 +46,29 @@ export default function SignUp() {
       <h1 className="text-3xl text-center font-semibold
       my-7">Sign Up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="text" placeholder="username"
-        className="border p-3 rounded-lg" id="username"
-        onChange={handleChange} />
-      
-        <input type="email" placeholder="email"
-        className="border p-3 rounded-lg" id="email" 
-        onChange={handleChange}/>
-      
-        <input type="password" placeholder="password"
-        className="border p-3 rounded-lg" id="password" 
-        onChange={handleChange}/>
-        <button disabled={loading} className="bg-slate-700 text-white p-3
-        rounded-lg uppercase hover:opacity-95
-         disabled:opacity-80">
+        <input
+         type="text" 
+         placeholder="username"
+         className="border p-3 rounded-lg"
+         id="username"
+         onChange={handleChange} 
+         />
+        <input 
+        type="email"  
+        placeholder="email"
+        className="border p-3 rounded-lg" 
+        id="email" 
+        onChange={handleChange}
+        />
+        <input 
+        type="password" 
+        placeholder="password"
+        className="border p-3 rounded-lg" 
+        id="password" 
+        onChange={handleChange}
+        />
+        <button disabled={loading} 
+        className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading...' : 'Sign Up'}
           </button>
       </form>
@@ -75,5 +80,5 @@ export default function SignUp() {
       </div>
       {error && <p className="text-red-500">{error}</p>}
     </div>
-  )
+  );
 }
